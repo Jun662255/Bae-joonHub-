b808c2abd893eb1bb68d4bbae1ef7bd4db461a87
Jun662255 <103052354+Jun662255@users.noreply.github.com>
2022-10-05 14:38:40 +0900

    [Bronze II] Title: 다이얼, Time: 196 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.next();
+
+		int c = 0;
+		for (int i = 0; i < s.length(); i++) {
+
+			switch (s.charAt(i)) {
+			case 'A':
+			case 'B':
+			case 'C':
+				c += 3;
+				break;
+			case 'D':
+			case 'E':
+			case 'F':
+				c += 4;
+				break;
+
+			case 'G':
+			case 'H':
+			case 'I':
+				c += 5;
+				break;
+
+			case 'J':
+			case 'K':
+			case 'L':
+				c += 6;
+				break;
+
+			case 'M':
+			case 'N':
+			case 'O':
+				c += 7;
+				break;
+
+			case 'P':
+			case 'Q':
+			case 'R':
+			case 'S':
+				c += 8;
+				break;
+
+			case 'T':
+			case 'U':
+			case 'V':
+				c += 9;
+				break;
+
+			case 'W':
+			case 'X':
+			case 'Y':
+			case 'Z':
+				c += 10;
+				break;
+			}
+		}
+	System.out.println(c);
+	}
+}
